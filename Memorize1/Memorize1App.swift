23ae59f09730805abbfa6c5804224dd7d7bed99b
Paul Ivanivsky <ivanivsky@gmail.com>
2024-10-02 07:04:43 -0400

    Initial Commit

---
+++ Memorize1/Memorize1App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize1App.swift
+//  Memorize1
+//
+//  Created by Paul Ivanivsky on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Memorize1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
